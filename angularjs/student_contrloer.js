5b3785975686acbbe2b075ebd2f72eac99d6d63c
latfur <rubel.itlive@gmail.com>
2017-10-08 11:17:23 +0600

    Update

--- angularjs/student_contrloer.js
+++ angularjs/student_contrloer.js
@@ -58,7 +58,7 @@ myApp.controller('student_contrloer', function ($scope, $state, $http, $location
     };
 
 
-    this.view_student_info = function (student_id) {
+    this.get_student_info = function (student_id) {
         $http.get(APP_CONSTANTS.BASE_URL+'/student/view_student_by_student_id?student_id=' + student_id).then(function (response) {
             vm.view_student_info = response.data;
 
